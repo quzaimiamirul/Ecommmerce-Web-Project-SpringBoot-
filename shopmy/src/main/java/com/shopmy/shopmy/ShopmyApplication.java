d8f182b82af7224c96b1818d558b6e7b468622a5
amirulquzaimi <amirulquzaimi.ptsb@gmail.com>
2025-05-13 19:09:20 +0800

    init commit

---
+++ shopmy/src/main/java/com/shopmy/shopmy/ShopmyApplication.java
@@ -0,0 +1,13 @@
+package com.shopmy.shopmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopmyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopmyApplication.class, args);
+	}
+
+}
---
+++ shopmy/src/test/java/com/shopmy/shopmy/ShopmyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopmy.shopmy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopmyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
